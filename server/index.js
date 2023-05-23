9bc378bde9ec53a41d0eddc83f24e9badd23add0
Gabriel <gabrielong2004@gmail.com>
2023-05-22 22:16:38 -0700

    Setup server

---
+++ server/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes/routes');
+const mongoString = process.env.DATABASE_URL;
+
+mongoose.connect(mongoString);
+const db = mongoose.connection;
+
+db.on('error', (error) => {
+    console.log(error);
+});
+
+db.once('connected', () => {
+    console.log('Database Connected');
+});
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/api', routes);
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000')
+});
---
+++ server/routes/routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const User = require('../models/User');
+
+const router = express.Router();
+
+router.post('/createUser', async (req, res) => {
+    console.log(req.body)
+    const data = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    try {
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave);
+    } catch (error) {
+        res.status(400).json({message: error.message});
+    }
+});
+
+module.exports = router;
