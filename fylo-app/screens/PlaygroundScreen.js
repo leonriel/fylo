7c02898e05297c8dec4a0de73397548b3bf6ac64
Gabriel <gabrielong2004@gmail.com>
2023-06-19 02:23:35 -0700

    Simplified styling

--- fylo-app/screens/PlaygroundScreen.js
+++ fylo-app/screens/PlaygroundScreen.js
@@ -1,48 +1,57 @@
-import { Text, StyleSheet, Image, Pressable } from "react-native";
+import { Text, StyleSheet, Image, Pressable, View, Dimensions } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { LinearGradient } from "expo-linear-gradient";
 
-const PlaygroundScreen = ({ user, sessions }) => {
+const PlaygroundScreen = ({ navigation, user, sessions }) => {
   return (
-    <SafeAreaView style={styles.container}>
-      <LinearGradient
-        colors={["#E37E56", "#DE8E3E", "#5EC4CD"]}
-        style={styles.gradient}
-      ></LinearGradient>
-      <SafeAreaView style={styles.banner}>
-        <SafeAreaView style={styles.profileContainer}>
-          <Image
-            source={require("../assets/adaptive-icon.png")}
-            style={styles.profileImage}
-          />
-        </SafeAreaView>
-        <SafeAreaView style={styles.logoContainer}>
-          <Image
-            source={require("../assets/icon.png")}
-            style={styles.logoImage}
-          />
-        </SafeAreaView>
-      </SafeAreaView>
-      <SafeAreaView style={styles.startContainer}>
-        <Text style={styles.startText}>Start</Text>
-        <SafeAreaView>
-          <Pressable>
-            <Image
-              source={require("../assets/adaptive-icon.png")}
-              style={styles.startImage}
-            />
-          </Pressable>
-        </SafeAreaView>
-        <Text style={styles.sessionText}>Session</Text>
-      </SafeAreaView>
-      {/* <Modal> */}
+    <View style={{height: Dimensions.get('window').height, width: Dimensions.get('window').width}}>
+        <LinearGradient
+            colors={["#E37E56", "#DE8E3E", "#5EC4CD"]}
+            style={styles.gradient}
+        >
+            <SafeAreaView style={styles.container}>
+                <View style={styles.banner}>
+                    <View style={styles.profileContainer}>
+                        <Image
+                            source={require("../assets/adaptive-icon.png")}
+                            style={styles.profileImage}
+                        />
+                    </View>
+                    <View style={styles.logoContainer}>
+                        <Image
+                            source={require("../assets/icon.png")}
+                            style={styles.logoImage}
+                        />
+                    </View>
+                    <View style={{flex: 1}} />
+                </View>
+                <View style={{justifyContent: "space-between", height: "100%"}}>
+                    <View style={{flex: 1}} />
+                    <View style={styles.startContainer}>
+                        <Text style={styles.startText}>Start</Text>
+                        <View>
+                        <Pressable>
+                            <Image
+                            source={require("../assets/adaptive-icon.png")}
+                            style={styles.startImage}
+                            />
+                        </Pressable>
+                        </View>
+                        <Text style={styles.sessionText}>Session</Text>
+                    </View>
+                    {/* <Modal> */}
 
-      <SafeAreaView style={styles.modalContainer}>
-        <Text style={styles.myFyloFlicsText}>my fyloflics</Text>
-        <Text style={styles.myFyloFlicsText}>^</Text>
-        <Text style={styles.swipeUpText}>swipe up</Text>
-      </SafeAreaView>
-    </SafeAreaView>
+                    <View style={{flex: 1, flexDirection: "row"}}>
+                        <View style={styles.modalContainer}>
+                            <Text style={styles.myFyloFlicsText}>my fyloflics</Text>
+                            <Text style={styles.myFyloFlicsText}>^</Text>
+                            <Text style={styles.swipeUpText}>swipe up</Text>
+                        </View>
+                    </View>
+                </View>
+            </SafeAreaView>
+        </LinearGradient>
+    </View>
 
     // </Modal>
   );
@@ -51,21 +60,22 @@ const PlaygroundScreen = ({ user, sessions }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    width: "100%",
-    position: "relative",
+    // width: "100%",
+    // position: "relative",
   },
 
   gradient: {
-    transform: [{ rotate: "-20deg" }],
-    position: "absolute",
-    width: 800,
-    height: 1000,
-    marginVertical: -80,
-    marginHorizontal: -150,
+    flex: 1
+    // transform: [{ rotate: "-20deg" }],
+    // position: "absolute",
+    // width: 800,
+    // height: 1000,
+    // marginVertical: -80,
+    // marginHorizontal: -150,
   },
 
   banner: {
-    display: "flex",
+    // display: "flex",
     flexDirection: "row",
     width: "100%",
   },
@@ -73,15 +83,16 @@ const styles = StyleSheet.create({
   profileContainer: {
     height: 47,
     width: 47,
-    alignItems: "center",
+    // alignItems: "center",
     justifyContent: "center",
+    flex: 1,
   },
 
   profileImage: {
     height: 47,
     width: 47,
     marginLeft: 30,
-    marginTop: 100,
+    // marginTop: 100,
     resizeMode: "contain",
     borderRadius: 47 / 2,
     borderWidth: 3,
@@ -89,23 +100,25 @@ const styles = StyleSheet.create({
   },
 
   logoContainer: {
-    width: "100%",
-    marginLeft: -47,
+    // width: "100%",
+    // marginLeft: -47,
+    flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    position: "relative",
-    display: "flex",
+    // position: "relative",
+    // display: "flex",
   },
 
   logoImage: {
-    marginTop: 65,
+    // marginTop: 65,
     height: 24.1,
     width: 74,
-    resizeMode: "contain",
+    // resizeMode: "contain",
   },
 
   startContainer: {
-    marginTop: 400,
+    // marginTop: 400,
+    flex: 1,
     justifyContent: "center",
     alignItems: "center",
     textAlign: "center",
@@ -113,7 +126,7 @@ const styles = StyleSheet.create({
 
   startText: {
     fontSize: 36,
-    marginTop: -150,
+    // marginTop: -150,
     color: "white",
     textAlign: "center",
     marginBottom: 5,
@@ -124,7 +137,7 @@ const styles = StyleSheet.create({
   startImage: {
     width: 168,
     height: 168,
-    borderRadius: 160 / 2,
+    borderRadius: 168 / 2,
     borderWidth: 5,
     borderColor: "white",
   },
@@ -139,8 +152,9 @@ const styles = StyleSheet.create({
   },
 
   modalContainer: {
-    position: "absolute",
-    bottom: 0,
+    // position: "absolute",
+    // bottom: 0,
+    // marginTop: 'auto',
     width: "100%",
     backgroundColor: "rgba(255, 255, 255, 0.3)",
     opacity: "30%",
@@ -150,6 +164,8 @@ const styles = StyleSheet.create({
     textAlign: "center",
     justifyContent: "center",
     alignItems: "center",
+    alignSelf: "flex-end",
+    marginBottom: 70 // Because of the tab bar >:(
   },
 
   myFyloFlicsText: {
@@ -163,7 +179,7 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: "white",
     fontFamily: "Quicksand-Bold",
-    fontWeight: 700,
+    // fontWeight: 700,
   },
 });
 
